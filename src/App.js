e02c446d8e03d1e6e2aebb39b3df851017c2a047
dimail0880 <dimail4888@gmail.com>
2020-04-30 15:42:17 +0300

    structure added

--- src/App.js
+++ src/App.js
@@ -1,26 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Keep calm and Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  static propTypes = {
+    // prop: PropTypes
+  }
+
+  render() {
+    return (
+      <div>
+        <p>тут будет домашка</p>
+      </div>
+    )
+  }
 }
-
-export default App;
